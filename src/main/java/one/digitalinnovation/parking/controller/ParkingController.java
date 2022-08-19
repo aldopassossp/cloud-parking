ff832766517d101270e7b1b013dfbef2ba7ff501
aldopassossp <aldopassos@gmail.com>
2022-08-19 14:29:49 -0300

    Adicionado primeiro EndPoint de Lista

--- src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
+++ src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
@@ -1,24 +1,32 @@
 package one.digitalinnovation.parking.controller;
 
+import one.digitalinnovation.parking.controller.DTO.ParkingDTO;
+import one.digitalinnovation.parking.controller.mapper.ParkingMapper;
 import one.digitalinnovation.parking.model.Parking;
+import one.digitalinnovation.parking.service.ParkingService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/parking")
 public class ParkingController {
 
+    private final ParkingService parkingService;
+    private final ParkingMapper parkingMapper;
+
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
+        this.parkingService = parkingService;
+        this.parkingMapper = parkingMapper;
+    }
+
     @GetMapping
-    public List<Parking> findAll(){
+    public List<ParkingDTO> findAll(){
 
-        var parking = new Parking();
-        parking.setColor("PRETO");
-        parking.setLicense("MSS-111");
-        parking.setModel("VW GOL");
-        parking.setState("SP");
-
-        return Arrays.asList(parking, parking);
+        List<Parking> parkingList = parkingService.findAll();
+        List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+        return result;
     }
 }
